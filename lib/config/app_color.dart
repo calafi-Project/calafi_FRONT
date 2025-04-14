a99e8059d4bb584b8adca8cf4129855641855089
soul071213 <js071213asd@gmail.com>
2025-04-14 10:36:31 +0900

    set : 환경 설정 완료

---
+++ lib/config/app_color.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  // 화이트
+  static Color white = const Color(0xffffffff);
+
+  // 레드
+  static Color red = const Color(0xFFD10119);
+
+  // 블루
+  static Color blue = const Color(0xff023474);
+
+  static Color talk = const Color(0xffFFC1C8);
+
+  static Color water = const Color(0xff2563EB);
+
+  // 그레이
+  static Color gray = const Color(0xffDDDDDD);
+  static Color gray50 = const Color(0xfff9fafb);
+  static Color gray100 = const Color(0xfff3f4f6);
+  static Color gray200 = const Color(0xffe5e7eb);
+  static Color gray300 = const Color(0xFfd1d5db);
+  static Color gray400 = const Color(0xFF9ca3af);
+  static Color gray500 = const Color(0xFF6b7280);
+  static Color gray600 = const Color(0xFF4b5563);
+  static Color gray700 = const Color(0xFF374151);
+  static Color gray800 = const Color(0xFF1f2937);
+  static Color gray900 = const Color(0xFF111827);
+  static Color gray950 = const Color(0xFF030712);
+}
